aa3bd48c2e7a98a224054c097205a1def64a882a
Calison <calisonsi@gmail.com>
2021-10-05 08:21:22 -0300

    Monta a struct baseado nos itens do githubstatus

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
 	"github.com/gocolly/colly"
+	"strings"
 )
 
+type Githubstats struct {
+	SystemName   string
+	Systemstatus bool
+}
+
 func main() {
-	collyRes := colly.NewCollector()
-	print(collyRes)
-	res, _ := http.Get("https://www.githubstatus.com/")
-	defer res.Body.Close()
-	content, _ := ioutil.ReadAll(res.Body)
-	contentAsString := string(content)
-	print(contentAsString)
+	collyCollector := colly.NewCollector(colly.Async(false))
+	var githubStatus []Githubstats
+	collyCollector.OnHTML("div[data-component-id]", func(element *colly.HTMLElement) {
+		element.ForEach("span", func(_ int, element *colly.HTMLElement) {
+			systemNameFromHtml := element.DOM.Filter(".name").Text()
+			systemStatusFromHtml := element.DOM.Filter(".component-status").Text()
+			if len(systemNameFromHtml) > 0 && !strings.Contains(systemNameFromHtml, "Visit") {
+				newGithubStatus := Githubstats{
+					SystemName:   strings.TrimSpace(systemNameFromHtml),
+					Systemstatus: strings.Contains(systemStatusFromHtml, "Operational"),
+				}
+				githubStatus = append(githubStatus, newGithubStatus)
+			}
+		})
+	})
+	collyCollector.Visit("https://www.githubstatus.com")
 }
