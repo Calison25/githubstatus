6aeb8ec2787ce547587330a5fad85bbb704ec0fe
Calison <calisonsi@gmail.com>
2021-10-05 22:19:30 -0300

    Finaliza o scrapping do githubstatus

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gocolly/colly"
 	"strings"
 )
@@ -11,20 +12,19 @@ type Githubstats struct {
 }
 
 func main() {
-	collyCollector := colly.NewCollector(colly.Async(false))
+	collyCollector := colly.NewCollector()
 	var githubStatus []Githubstats
 	collyCollector.OnHTML("div[data-component-id]", func(element *colly.HTMLElement) {
-		element.ForEach("span", func(_ int, element *colly.HTMLElement) {
-			systemNameFromHtml := element.DOM.Filter(".name").Text()
-			systemStatusFromHtml := element.DOM.Filter(".component-status").Text()
-			if len(systemNameFromHtml) > 0 && !strings.Contains(systemNameFromHtml, "Visit") {
-				newGithubStatus := Githubstats{
-					SystemName:   strings.TrimSpace(systemNameFromHtml),
-					Systemstatus: strings.Contains(systemStatusFromHtml, "Operational"),
-				}
-				githubStatus = append(githubStatus, newGithubStatus)
+		systemNameFromHtml := element.DOM.Find("span.name").Text()
+		systemStatusFromHtml := element.DOM.Find("span.component-status").Text()
+		if len(systemNameFromHtml) > 0 && !strings.Contains(systemNameFromHtml, "Visit") {
+			newGithubStatus := Githubstats{
+				SystemName:   strings.TrimSpace(systemNameFromHtml),
+				Systemstatus: strings.Contains(systemStatusFromHtml, "Operational"),
 			}
-		})
+			githubStatus = append(githubStatus, newGithubStatus)
+		}
 	})
 	collyCollector.Visit("https://www.githubstatus.com")
+	fmt.Println(githubStatus)
 }
